5ae07674697982502d92b1ed0354d287090ecaaa
winlingit <winlin@gmail.com>
2017-03-11 23:48:08 -0600

    add preprocess function for transcripts.wikia scripts

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ import re
 import nltk
 import itertools
 
+# strip markdown, new line chars
+def preprocess(line):
+    strip = ["*", "'''", "\n"]
+    
+    for s in strip:
+        line = line.replace(s, "")
+    return line.split(": ")
