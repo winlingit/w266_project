9c199f6c15ee6f8e63adfd32adb2b0853ddb03a6
winlingit <winlin@gmail.com>
2017-03-19 18:05:15 -0500

    finished preprocessing for scripts from transcripts.wikia.  added lowercase, narrator as speaker for bracketed text, merged dialogue fragments for files with new lines between sentences.  some scripts have narration embedded in dialogue, still need to address this...

--- utils.py
+++ utils.py
@@ -4,9 +4,3 @@ import nltk
 import itertools
 
 # strip markdown, new line chars
-def preprocess(line):
-    strip = ["*", "'''", "\n"]
-    
-    for s in strip:
-        line = line.replace(s, "")
-    return line.split(": ")
