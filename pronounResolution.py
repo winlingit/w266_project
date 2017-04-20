25a9136f8bedb78d2d5c8b2639d349e701110218
jimscchen <jim.chen@ischool.berkeley.edu>
2017-04-19 22:44:08 -0500

    modify pronoun baseline

--- pronounResolution.py
+++ pronounResolution.py
@@ -4,15 +4,16 @@ import numpy as np
 import pandas as pd
 import os
 
-def pronResolution(cList, tokens):
+def pronResolution_base(cList, rows):
     '''
     cList is a list of characters(str) that appear in the movie
     tokens are from the processed csv files
     expect the function to add a char tag to pronouns of interest, 
     either matching it to a name directly in cList, or another reasonable entity
+    baseline randomly associates pronouns to the list of characters
     '''
-    for token in tokens:
+    for token in rows.tokens:
         #print(token)
         if token['pos'] == 'PRON':
-            token['char'] = 'narrator'
-    return tokens
+            token['char'] = np.random.choice(cList)
+    return rows.tokens
