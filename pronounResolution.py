96e3d11d87c169981929bfb566e5997b0efc7919
jimscchen <jim.chen@ischool.berkeley.edu>
2017-04-20 19:12:45 -0500

    provide eval function

--- pronounResolution.py
+++ pronounResolution.py
@@ -13,14 +13,64 @@ def pronResolution_base(cList, rows):
     baseline randomly associates pronouns to the list of characters
     {char: [A, B, ...]} (using a list to handle possible issue with plural pronouns
     '''
+    hasChar = False
     for token in rows.tokens:
         #print(token)
         if token['pos'] == 'PRON':
             token['char'] = [np.random.choice(cList)]
-    return rows.tokens
+            hasChar = True
+            
+    return rows.tokens, hasChar
 
 def pronResolution_sent(cDict, rows):
     '''
     cDict is a dictionary of characters and their total sentiment values in the movie
     '''
     return rows.tokens
+
+def pronEval(dfList, num=50):
+    '''
+    This function takes a list of dfs and evaluate model performance
+    Enhance the original google api df with one of the model functions
+    '''
+    numModels = len(dfList)
+    sampled = np.zeros(numModels)
+    correct = np.zeros(numModels)
+    
+    for i in range(num*numModels):
+        selectModel = np.random.choice(range(numModels))
+        
+        df = dfList[selectModel]
+        charIndex = list(df[df.hasChar == True].index)
+        selectLine = np.random.choice(charIndex)
+        charList = [(x['content'], x['char']) for x in df.loc[selectLine]['tokens'] if 'char' in x]
+        print('*******for the following dialogue********')
+        for rowNum in range(max(0, selectLine - 2), min(len(dfList[selectModel]), selectLine + 3)):
+            if rowNum == selectLine:
+                print('=>{}: {}<='.format(df.loc[rowNum]['speaker'], df.loc[rowNum]['dialogue']))
+            else:
+                print('{}: {}'.format(df.loc[rowNum]['speaker'], df.loc[rowNum]['dialogue']))
+        print('*'*20)
+        print('{} pronouns resolved'.format(len(charList)))
+        for i, char in enumerate(charList):
+            print('{}. {} => {}'.format(i+1, char[0], char[1]))
+        
+        collectInput = False
+        
+        while not collectInput:
+            try:
+                count = int(input('how many are correctly identified?'))
+                collectInput = True
+            except:
+                print('incorrect input, only numbers allowed')
+        
+        sampled[selectModel] += len(charList)
+        correct[selectModel] += count
+
+    print('*'*20)
+    for i, modelResult in enumerate(zip(correct, sampled)):
+        if modelResult[0] == 0:
+            result = 0
+        else:
+            result = modelResult[0]/modelResult[1]
+        print('model {} precision: {}'.format(i+1, result))
